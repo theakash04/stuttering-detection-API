81465bdff59170bf09f71910190f813f97615689
theakash04 <aakashkkr04@gmail.com>
2025-02-18 21:56:24 +0530

    small change in backend

--- Backend/main.py
+++ Backend/main.py
@@ -36,39 +36,39 @@ class WhisperForStutteringClassification(nn.Module):
         return logits
 
 # file path of trained model
-# model_path = "/stuttering_detetction/model/whisper-small/stuttering_detection_model.pth"
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-#
-# model = WhisperForStutteringClassification()
-# model.load_state_dict(torch.load(model_path, map_location=device))
-# model.to(device)
-# model.eval()
-#
-# feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
-#
-# def preprocess_audio(audio_path):
-#     waveform, sample_rate = torchaudio.load(audio_path)
-#     if sample_rate != 16000:
-#         transform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)
-#         waveform = transform(waveform)
-#     input_features = feature_extractor(
-#         waveform.numpy(),
-#         sampling_rate=16000,
-#         return_tensors="pt"
-#     ).input_features
-#
-#     return input_features.to(device)
-#
-# def predict_stuttering(audio_path):
-#     input_features = preprocess_audio(audio_path)
-#
-#     with torch.no_grad():
-#         logits = model(input_features)
-#
-#     probabilities = torch.softmax(logits, dim=1)
-#     prediction = torch.argmax(probabilities, dim=1).cpu().numpy()[0]
-#
-#     return "Stuttering Detected" if prediction == 1 else "No Stuttering"
+model_path = "/stuttering_detetction/model/whisper-small/stuttering_detection_model.pth"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+model = WhisperForStutteringClassification()
+model.load_state_dict(torch.load(model_path, map_location=device))
+model.to(device)
+model.eval()
+
+feature_extractor = WhisperFeatureExtractor.from_pretrained("openai/whisper-small")
+
+def preprocess_audio(audio_path):
+    waveform, sample_rate = torchaudio.load(audio_path)
+    if sample_rate != 16000:
+        transform = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)
+        waveform = transform(waveform)
+    input_features = feature_extractor(
+        waveform.numpy(),
+        sampling_rate=16000,
+        return_tensors="pt"
+    ).input_features
+
+    return input_features.to(device)
+
+def predict_stuttering(audio_path):
+    input_features = preprocess_audio(audio_path)
+
+    with torch.no_grad():
+        logits = model(input_features)
+
+    probabilities = torch.softmax(logits, dim=1)
+    prediction = torch.argmax(probabilities, dim=1).cpu().numpy()[0]
+
+    return "Stuttering Detected" if prediction == 1 else "No Stuttering"
 
 
 def generate_content(stuttering: str, api_key: str) -> dict:
